21a4f62004745d728c1d9c553c5f64ea7a301652
Jordi Morillo <jmdesarrollo82@gmail.com>
2024-04-10 13:27:05 +0200

    feature: add the explanation for single responsibility principle

---
+++ 0_single_responsibility.php
@@ -0,0 +1,31 @@
+<?php
+
+/** NOT APPLYING SOLID */
+public function an_orchestra_man()
+{
+    echo "I'm a chef";
+    echo "I'm a gardener";
+    echo "I'm a painter";
+    echo "I'm a driver";
+}
+
+/** APPLYING SOLID */
+public function a_chef()
+{
+    echo "I'm a chef";
+}
+
+public function a_gardener()
+{
+    echo "I'm a gardener";
+}
+
+public function a_painter()
+{
+    echo "I'm a painter";
+}
+
+public function a_driver()
+{
+    echo "I'm a driver";
+}
